5916030895e7e5be42549eb08864319347c858c3
kshyun0415 <71251902+kshyun0415@users.noreply.github.com>
2023-02-01 18:34:38 +0900

    Pawn Script

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -10,7 +10,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof(CharacterController))]
     [RequireComponent(typeof(AudioSource))]
-    public class FirstPersonController : MonoBehaviour
+    public class FirstPersonController : LivingEntity
     {
         [SerializeField] private bool m_IsWalking;
         [SerializeField] private float m_WalkSpeed;
